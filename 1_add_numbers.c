85b9915fe2a90b5ace72d218133a4c15ae0234a5
Maryam Taghi Zadeh <mtaghizadeh2023@fau.edu>
2023-08-25 01:57:30 +0100

    initial program, first c code

---
+++ 1_add_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b , number ,addition;
+	printf("hello please enter a number\n");
+	scanf("%d", &a);
+	printf("place another number \n");
+	scanf("%d" , &b);
+	addition=a+b;
+	printf("the answer in addition is %d", addition);
+return 0;
+}
