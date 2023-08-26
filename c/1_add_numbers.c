b2898430811d13baf830b54c7d811505a562135b
nickumia <nickumia6@gmail.com>
2023-08-26 01:38:25 -0400

    refactor: make text more user friendly
    just some capitalization and punctuation ;)

--- c/1_add_numbers.c
+++ c/1_add_numbers.c
@@ -6,16 +6,16 @@ int main() {
 	int a, b, number, addition;
 
   // Ask for first number
-	printf("hello please enter a number\n");
+	printf("Hello!  Please enter a number: ");
 	scanf("%d", &a);
 
   // Ask for second number
-	printf("place another number \n");
+	printf("Please enter another number: ");
 	scanf("%d" , &b);
 
   // Add two numbers and display result
-	addition=a+b;
-	printf("the answer in addition is %d", addition);
+	addition = a + b;
+	printf("The sum of the two numbers is %d.\n", addition);
 
   // Program exits successfully
   return 0;
